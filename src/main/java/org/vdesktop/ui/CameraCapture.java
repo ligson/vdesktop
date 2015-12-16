f68a34f546b7784b4bce26a1a12dc7e67556fc0a
ligson <593949938@qq.com>
2015-12-16 23:24:11 +0800

    视频摄像头

--- src/main/java/org/vdesktop/ui/CameraCapture.java
+++ src/main/java/org/vdesktop/ui/CameraCapture.java
@@ -6,11 +6,29 @@
 
 package org.vdesktop.ui;
 
+import org.bytedeco.javacpp.opencv_core.IplImage;
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
 
 public class CameraCapture {
-	public static void main(String[] args) throws Exception{
-		OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(-1);
+	public static void main(String[] args) throws Exception {
+		/*
+		 * OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+		 * grabber.start();
+		 */
+		FrameGrabber grabber = FrameGrabber.createDefault(0);
+		grabber.setFormat("video4linux2");
 		grabber.start();
+		Frame grabbedImage = grabber.grab();
+		CanvasFrame frame = new CanvasFrame("视频格式",
+				CanvasFrame.getDefaultGamma() / grabber.getGamma());
+		while (frame.isVisible() && (grabbedImage = grabber.grab()) != null) {
+			frame.showImage(grabbedImage);
+		}
+		frame.dispose();
+		grabber.stop();
 	}
 }
